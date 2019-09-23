b8acda67502f4c14ab085b5ecb9b8e09f2986dc5
mwalidnurfadhil22 <173040079@mail.unpas.ac.id>
2019-09-23 19:48:30 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non accusantium repellendus nam, iste voluptatem praesentium. Non unde corporis quae quisquam consectetur animi cumque architecto et laborum, laboriosam voluptatibus veniam magnam!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
